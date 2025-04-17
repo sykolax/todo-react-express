11b29a008e0e3521bb7e9f08ba491af0a29ea1ef
syko <sykolax@gmail.com>
2025-04-16 23:37:24 -0400

    init: initial setup for express

---
+++ server/src/config/config.ts
@@ -0,0 +1,16 @@
+// loads environment variables from .env and provides type checking
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface Config {
+    port: number;
+    nodeEnv: string;
+}
+
+const config: Config = {
+    port: Number(process.env.PORT) || 3000, 
+    nodeEnv: process.env.NODE_ENV || 'development',
+};
+
+export default config;
