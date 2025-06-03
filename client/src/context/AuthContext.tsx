d7f4def6b99d14c89c980321c8d229677e024223
syko <sykolax@gmail.com>
2025-06-02 23:58:59 -0400

    feat: add logout function

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -1,22 +1,41 @@
 import { createContext, useContext, useState } from 'react';
+import api from '@/lib/axios';
 
 type AuthContextType = {
     isLoggedIn: boolean;
     setIsLoggedIn: (value: boolean) => void;
     username: string;
     setUsername: (value: string) => void;
+    isLoading: boolean;
+    setIsLoading: (value: boolean) => void;
+    logout: () => void;
 }
 
 // Outside of the provider, you get undefined
-// Use it 
 const AuthContext = createContext<AuthContextType|undefined>(undefined);
 
-export function AuthProvider({children}: {children: React.ReactNode}) {
+export function AuthProvider({ children }: { children: React.ReactNode }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [username, setUsername] = useState('');
+    const [isLoading, setIsLoading] = useState(true);
+
+    const logout = async () => {
+        try {
+            setIsLoading(true);
+            const response = api.post('auth/logout');
+            console.log(response);
+            setIsLoggedIn(false);
+            setUsername('');
+
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
-        <AuthContext.Provider value={{isLoggedIn, setIsLoggedIn, username, setUsername}}>
+        <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, username, setUsername, isLoading, setIsLoading, logout }}>
             {children}
         </AuthContext.Provider>
     );
