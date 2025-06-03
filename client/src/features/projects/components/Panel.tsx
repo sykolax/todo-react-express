987ff6bab5486c14e7e04c8ac3a3c7fb6364f31c
syko <sykolax@gmail.com>
2025-06-03 00:05:14 -0400

    feat: CRUD project and task

--- client/src/features/projects/components/Panel.tsx
+++ client/src/features/projects/components/Panel.tsx
@@ -4,90 +4,231 @@ import checkIcon from '@assets/check_icon.svg';
 import addIcon from '@assets/add_icon.svg';
 import uncheckedIcon from '@assets/checkbox_unchecked.svg';
 import checkedIcon from '@assets/checkbox_checked.svg';
+import { useRef, useState } from 'react';
+import type { TaskResponse } from './ProjectPagePanel';
 
-type ItemProps = {
+export type ItemProps = {
     id: number;
     label: string;
     checked?: boolean;
+    onClick?: React.MouseEventHandler<HTMLDivElement>;
+}
+
+export type ProjectRowProps = {
+    id: number;
+    title: string;
+    setCurrentProjectId: (pid: number) => void;
+    editHandler: (pid: number, newTitle: string) => void;
+    deleteHandler: (pid: number) => void;
+}
+
+export type TaskRowProps = {
+    id: number;
+    description: string;
+    completed: boolean;
+    editHandler: (tid: number, newFields: Partial<TaskResponse>) => void;
+    deleteHandler: (tid: number) => void;
+}
+
+type EditableRecordProps = {
+    value: string;
+    name: string;
+    submitHandler: (val: string) => void;
+    formRef: React.Ref<HTMLFormElement>;
 }
 
 type ItemListProps = {
-    items: ItemProps[];
-    Row: React.ComponentType<{ itemProps: ItemProps }>;
+    items: ProjectRowProps[] | TaskRowProps[];
+    type: PanelType;
+}
+
+type ItemInputProps = {
+    placeholder: string;
+    onSubmit: (val: string) => void; 
+}
+
+type IconButtonProps = {
+    icon: string;
+    onClick?: () => void;
+    type?: "submit" | "reset" | "button" | undefined;
+}
+
+type RowButtonSetProps = {
+    checkOnClick: () => void;
+    deleteOnClick: () => void; 
+    isEditMode: boolean;   
+    setEditMode: (val: boolean) => void;
 }
 
 type PanelType = "project" | "task";
 
-type PanelProps = {
+export type PanelProps = {
     type: PanelType;
-    items: ItemProps[];
+    items: ProjectRowProps[] | TaskRowProps[];
     title: string; 
     inputPlaceholder: string;
+    submitHandler: (val:string) => void;
+    projectClickHandler?: (pid: number) => void;
     className?: string;
 }
 
-function IconButton({ icon }: { icon: string }) {
+function IconButton({ icon, onClick, type}: IconButtonProps ) {
+
+    function handleClick(event: React.MouseEvent<HTMLButtonElement>) {
+        event.stopPropagation();
+        if (onClick) {
+            onClick();
+        }
+    }
+
     return (
-        <button><img src={icon}/></button>
+        <button onClick={handleClick} type={type}><img src={icon}/></button>
     );
 }
 
-function RowButtonSet() {
+function RowButtonSet({ checkOnClick, deleteOnClick, isEditMode, setEditMode }: RowButtonSetProps) {
+
+    function handleCheckClick() {
+        setEditMode(false);
+        checkOnClick();
+    }
+
+    function handleEditClick() {
+        setEditMode(true);
+    }
+
     return (
-            <div className="flex gap-3">
-                <IconButton icon={checkIcon}/>
-                <IconButton icon={editIcon}/>
-                <IconButton icon={trashIcon}/>
+            <div className="flex gap-3 shrink-0">
+                { isEditMode && <IconButton icon={checkIcon} onClick={handleCheckClick} /> }
+                { !isEditMode && <IconButton icon={editIcon} onClick={handleEditClick}/> }
+                <IconButton icon={trashIcon} onClick={deleteOnClick}/> 
             </div>
     );
 }
 
-function ProjectRow ({ itemProps }: { itemProps: ItemProps }) {
+function EditableRecord({ value, name, submitHandler, formRef }: EditableRecordProps) {
+    const [inputValue, setInputValue] = useState(value);
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        submitHandler(inputValue);
+    }
+
     return (
-        <div className="bg-stone-100 px-5 py-3 w-full text-black flex justify-between">
-            { itemProps.label }
-            <RowButtonSet />
+        <form className="w-full mr-3" onSubmit={handleSubmit} ref={formRef}>
+            <input autoFocus className="w-full" type="text" value={inputValue} name={name} onChange={(e) => setInputValue(e.target.value)}/>
+        </form>
+    );
+}
+
+function ProjectRow ({ projectProps }: { projectProps: ProjectRowProps }) {
+    const [isEditMode, setIsEditMode] = useState(false);
+    const formRef = useRef<HTMLFormElement>(null);
+
+    function handleClick() {
+        projectProps.setCurrentProjectId(projectProps.id);
+    }
+
+    function handleConfirmClick() {
+        // triggers the editable record form submit -> handleRecordSubmit(edit the project)
+        formRef.current?.requestSubmit();
+    }
+
+    function handleRecordSubmit(inputValue: string) {
+        console.log(projectProps);
+        projectProps.editHandler(projectProps.id, inputValue);
+    }
+
+    function handleDeleteClick() {
+        projectProps.deleteHandler(projectProps.id);
+    }
+
+    return (
+        <div className="bg-stone-100 px-5 py-3 w-full text-black flex justify-between cursor-pointer mb-3" onClick={handleClick}>
+            { isEditMode ? 
+            <EditableRecord value={projectProps.title} name={`project-${projectProps.id}`} submitHandler={handleRecordSubmit} formRef={formRef} /> : 
+            projectProps.title }
+            <RowButtonSet checkOnClick={handleConfirmClick} deleteOnClick={handleDeleteClick} isEditMode={isEditMode} setEditMode={setIsEditMode}/>
         </div>
     );
 }
 
-function TaskRow ({ itemProps }: { itemProps: ItemProps }) {
+function TaskRow ({ taskProps }: { taskProps: TaskRowProps }) {
+    const [isEditMode, setIsEditMode] = useState(false);
+    const formRef = useRef<HTMLFormElement>(null);
+
+    function handleCheck() {
+        taskProps.editHandler(taskProps.id, {completed: true});
+    }
+
+    function handleUncheck() {
+        taskProps.editHandler(taskProps.id, {completed: false});
+    }
+
+    function handleConfirmClick() {
+        formRef.current?.requestSubmit();
+    }
+
+    function handleDeleteClick() {
+        taskProps.deleteHandler(taskProps.id);
+    }
+
+    function handleRecordSubmit(inputValue: string) {
+        taskProps.editHandler(taskProps.id, {description: inputValue});
+    }
+
     return (
-        <div className="bg-stone-100 px-5 py-3 w-full text-black flex justify-between">
+        <div className="bg-stone-100 px-5 py-3 w-full text-black flex justify-between mb-3">
             <div className="flex gap-3">
-                <IconButton icon={uncheckedIcon} />
-                { itemProps.label }
+                { taskProps.completed ? <IconButton icon={checkedIcon} onClick={handleUncheck} /> : <IconButton icon={uncheckedIcon} onClick={handleCheck} /> }
+                { isEditMode ? 
+                <EditableRecord value={taskProps.description} name={`task-${taskProps.id}`} submitHandler={handleRecordSubmit} formRef={formRef} /> : 
+                taskProps.description }
             </div>
-            <RowButtonSet />
+            <RowButtonSet checkOnClick={handleConfirmClick} deleteOnClick={handleDeleteClick} isEditMode={isEditMode} setEditMode={setIsEditMode}/>
         </div>
     );
 }
 
-function ItemList({ items, Row }: ItemListProps ) {
+function ItemList({ items, type }: ItemListProps ) {
     return (
-        <div className="flex flex-col gap-3">
-            {items.map(item => <Row key={item.id} itemProps={item} />)}
+        <div className="flex flex-col">
+            { type === "project" &&
+            (items as ProjectRowProps[]).map(item => (
+                <ProjectRow key={item.id} projectProps={item} />
+            ))}
+            { type === "task" &&
+            (items as TaskRowProps[]).map(item => (
+                <TaskRow key={item.id} taskProps={item} />
+            ))}
         </div>
     );
 }
 
-function ItemInput({ placeholder }: { placeholder: string }) {
+function ItemInput({ placeholder, onSubmit }: ItemInputProps) {
+    const [inputValue, setInputValue] = useState('');
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        onSubmit(inputValue);
+        setInputValue('');
+    }
+    
     return (
-        <div className="flex justify-between gap-3 mt-4">
-            <input className="bg-stone-100 px-5 py-3 grow rounded-3xl" type="text" placeholder={placeholder}/>
-            <IconButton icon={addIcon} />
-        </div>
+        <form onSubmit={handleSubmit} className="flex justify-between gap-3">
+            <input className="bg-stone-100 px-5 py-3 grow rounded-3xl" type="text" name="inputData" placeholder={placeholder} value={inputValue} onChange={(e)=> setInputValue(e.target.value)}/>
+            <IconButton icon={addIcon} type="submit"/>
+        </form>
     );
 }
 
-export default function Panel({ type, items, title, inputPlaceholder, className }: PanelProps) {
-
+export default function Panel({ type, items, title, inputPlaceholder, submitHandler, className }: PanelProps) {
     return (
         <>
-            <div className={"text-left "+className}>
+            <div className={"text-left " + className}>
                 <h2 className="text-xl mb-3">{title}</h2>
-                <ItemList items={items} Row={ type === "project" ? ProjectRow : TaskRow }/>
-                <ItemInput placeholder={inputPlaceholder}/>
+                <ItemList items={items} type={type}/>
+                <ItemInput placeholder={inputPlaceholder} onSubmit={submitHandler}/>
             </div>
         </>
     );
